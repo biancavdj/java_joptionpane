789a72e840a1f13997e39084294096bbb45ab09e
Bianca <108751707+biveronez@users.noreply.github.com>
2024-04-03 20:57:39 -0300

    Add files via upload

---
+++ IdadeAnos/src/App.java
@@ -0,0 +1,36 @@
+/*
+ * Fazer um programa que colocaremos o ano de nascimento e o ano atual 
+ * para calcular a idade em anos, meses, dias e semanas
+ */
+
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+     //declaração das variáveis
+
+     int idadeAnos, anoAtual, anoNasc, idadeMeses, idadeDias, idadeSemanas;
+
+     //atribuir o ano atual e o ano de nascimento 
+
+     anoAtual = 2024;
+     anoNasc = 2004;
+
+     //calcular as idades 
+
+     idadeAnos = anoAtual - anoNasc;
+     idadeMeses = idadeAnos * 12;
+     idadeDias = idadeAnos * 365;
+     idadeSemanas = idadeDias / 7; 
+
+     // saída de dados 
+
+     JOptionPane.showMessageDialog(null,
+     "\n Idade em anos: " + idadeAnos +
+     "\n Idade em meses: " + idadeMeses +
+     "\n Idade em semanas: " + idadeSemanas +
+     "\n Idade em dias: " + idadeDias);
+
+    }
+}
